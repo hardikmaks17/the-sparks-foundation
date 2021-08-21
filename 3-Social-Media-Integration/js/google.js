ba29fd5ff5dcf526ca0117fea8871a1b4d8a70fd
Hardik Makwana <hardikmakwana1432@gmail.com>
2021-08-21 15:34:12 +0530

    displayed google profile

--- 3-Social-Media-Integration/js/google.js
+++ 3-Social-Media-Integration/js/google.js
@@ -3,6 +3,16 @@ console.log("Linked: google.js");
 // signin
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
+
+    let gId = document.getElementById('g-id');
+    gId.innerText = profile.getId();
+    let gName = document.getElementById('g-name');
+    gName.innerText = profile.getName();
+    let gEmail = document.getElementById('g-email');
+    gEmail.innerText = profile.getEmail();
+    let gImage = document.getElementById('g-image');
+    gImage.src = `${profile.getImageUrl()}`;
+
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
