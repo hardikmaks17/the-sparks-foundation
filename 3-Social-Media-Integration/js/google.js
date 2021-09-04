b0612f82761ade7c7ee46c7c02948ede808fcb94
Hardik Makwana <hardikmakwana1432@gmail.com>
2021-09-04 10:33:22 +0530

    Update google.js

--- 3-Social-Media-Integration/js/google.js
+++ 3-Social-Media-Integration/js/google.js
@@ -3,13 +3,15 @@ console.log("Linked: google.js");
 // signin
 function onSignIn(googleUser) {
     console.log('User signed in.');
-
+    
     // console massages
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    
+    document.getElementById('status').innerHTML = 'Thanks for logging in, ' + profile.getName() + '!';
     
     // creating user profile for DOM
     let profileBody = document.getElementById('profile-body');
